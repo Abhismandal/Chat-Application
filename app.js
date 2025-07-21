2a30106eb3d5e167729a0136931d85b7e4bba34a
Gaurav Gupta <gauravgupta157204@gmail.com>
2025-07-21 17:31:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 5000;
+
+// Serve static files from "public" folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('send message', (chat) => {
+    io.emit('send message', chat); // Broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
